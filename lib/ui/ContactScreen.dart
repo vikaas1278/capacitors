086fed4536bcd8f7c6ef39c580bafe7b2eed74ff
dev656 <devenderaspldelhi@gmail.com>
2020-09-15 15:00:01 +0530

    Initial commit

---
+++ lib/ui/ContactScreen.dart
@@ -0,0 +1,61 @@
+import 'package:capacitors/app/ServiceLocator.dart';
+import 'package:capacitors/modal/KonnectDetails.dart';
+import 'package:capacitors/utils/CarouselSlider.dart';
+import 'package:capacitors/utils/KonnectButton.dart';
+
+import './base/libraryExport.dart';
+
+class ContactScreen extends StatelessWidget {
+  final UrlLauncherService _service = locator<UrlLauncherService>();
+  final KonnectDetails konnectDetails;
+
+  ContactScreen(this.konnectDetails);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<CoverImage> _coverImage = konnectDetails.coverImage;
+    final BasicInfo _basicInfo = konnectDetails.basicInfo;
+    return Scaffold(
+      appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios, color: Colors.white),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: Text("Contact")),
+      body: ListView(children: <Widget>[
+        CarouselSlider(images: _coverImage, info: _basicInfo),
+        Padding(
+          padding: EdgeInsets.only(top: 24, left: 12, right: 12),
+          child: KonnectButton(
+            icon: Icon(Icons.call, color: Colors.grey),
+            label: Text(
+              _basicInfo.mobileNumber,
+            ),
+            onPressed: () {
+              if (_basicInfo.mobileNumber.length > 9) {
+                _service.call(_basicInfo.mobileNumber);
+              }
+            },
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 24, left: 12, right: 12),
+          child: KonnectButton(
+            icon: Icon(Icons.email, color: Colors.grey),
+            label: Text(
+              _basicInfo.email,
+            ),
+            onPressed: () {
+              if (_basicInfo.email.length > 9) {
+                _service.sendEmail(_basicInfo.email);
+              }
+            },
+          ),
+        ),
+        SizedBox(height: 24),
+        Divider(thickness: 16),
+
+      ]),
+    );
+  }
+}
---
+++ lib/ui/base/libraryExport.dart
@@ -0,0 +1,91 @@
+export 'package:capacitors/app/ApiAdmin.dart';
+export 'package:capacitors/app/ApiClient.dart';
+export 'package:capacitors/app/HttpClient.dart';
+export 'package:capacitors/app/AppConstants.dart';
+export 'package:capacitors/app/AppPreferences.dart';
+export 'package:capacitors/app/ServiceLocator.dart';
+
+export 'package:capacitors/modal/CartSummery.dart';
+export 'package:capacitors/modal/ProductDetails.dart';
+export 'package:capacitors/modal/KonnectDetails.dart';
+export 'package:capacitors/modal/UserAdmin.dart';
+export 'package:capacitors/modal/UserProfile.dart';
+export 'package:capacitors/modal/UserLoginType.dart';
+
+export 'package:capacitors/ui/admin/AdminDashboard.dart';
+export 'package:capacitors/ui/admin/AdminLedger.dart';
+export 'package:capacitors/ui/admin/AdminPayReceipt.dart';
+export 'package:capacitors/ui/admin/AdminProformaInvoice.dart';
+export 'package:capacitors/ui/admin/AdminPurchaseInvoice.dart';
+export 'package:capacitors/ui/admin/AdminSalesInvoice.dart';
+export 'package:capacitors/ui/admin/AdminPartyMaster.dart';
+export 'package:capacitors/ui/admin/AdminSalesOrder.dart';
+
+export 'package:capacitors/utils/CarouselSlider.dart';
+export 'package:capacitors/utils/DashboardButton.dart';
+export 'package:capacitors/utils/GroupedCheckbox.dart';
+export 'package:capacitors/utils/KonnectButton.dart';
+export 'package:capacitors/utils/PinchZoomImage.dart';
+export 'package:capacitors/utils/RadioButtonGroup.dart';
+
+export 'package:capacitors/ui/AdminLogin.dart';
+export 'package:capacitors/ui/AboutScreen.dart';
+export 'package:capacitors/ui/BankingScreen.dart';
+export 'package:capacitors/ui/ContactScreen.dart';
+export 'package:capacitors/ui/DashboardScreen.dart';
+export 'package:capacitors/ui/GalleryScreen.dart';
+export 'package:capacitors/ui/LocationScreen.dart';
+export 'package:capacitors/ui/AdminLogin.dart';
+export 'package:capacitors/ui/OffersScreen.dart';
+export 'package:capacitors/ui/RegistrationScreen.dart';
+export 'package:capacitors/ui/SupportScreen.dart';
+export 'package:capacitors/ui/WebStoreScreen.dart';
+
+export 'package:capacitors/ui/link/LinkUserDashboard.dart';
+export 'package:capacitors/ui/link/LinkUserLedger.dart';
+export 'package:capacitors/ui/link/LinkUserPartyMaster.dart';
+export 'package:capacitors/ui/link/LinkUserPayReceipt.dart';
+export 'package:capacitors/ui/link/LinkUserProformaInvoice.dart';
+export 'package:capacitors/ui/link/LinkUserSalesOrder.dart';
+export 'package:capacitors/ui/link/LinkUserSalesInvoice.dart';
+
+export 'package:capacitors/ui/pm/PartyMasterAddPayment.dart';
+export 'package:capacitors/ui/pm/PartyMasterDashboard.dart';
+export 'package:capacitors/ui/pm/PartyMasterInvoice.dart';
+export 'package:capacitors/ui/pm/PartyMasterLedger.dart';
+export 'package:capacitors/ui/pm/PartyMasterPaymentQr.dart';
+export 'package:capacitors/ui/pm/PartyMasterPayment.dart';
+export 'package:capacitors/ui/pm/PartyMasterSalesOrder.dart';
+
+export 'package:capacitors/ui/pm/profile/PartyMasterForgot.dart';
+export 'package:capacitors/ui/pm/profile/PartyMasterLogin.dart';
+export 'package:capacitors/ui/pm/profile/PartyMasterMobile.dart';
+export 'package:capacitors/ui/pm/profile/PartyMasterRegister.dart';
+export 'package:capacitors/ui/pm/profile/PartyMasterUpdate.dart';
+
+export 'package:capacitors/ui/store/OrderCart.dart';
+export 'package:capacitors/ui/store/OrderSummery.dart';
+export 'package:capacitors/ui/store/ProductDetail.dart';
+export 'package:capacitors/ui/store/ProductList.dart';
+export 'package:capacitors/ui/store/ProductSearch.dart';
+export 'package:capacitors/ui/store/WebViewFlutter.dart';
+
+export 'package:capacitors/ui/view/LedgerView.dart';
+export 'package:capacitors/ui/view/PartyMasterView.dart';
+export 'package:capacitors/ui/view/PayReceiptView.dart';
+export 'package:capacitors/ui/view/ProformaInvoiceView.dart';
+export 'package:capacitors/ui/view/PurchaseInvoiceView.dart';
+export 'package:capacitors/ui/view/SalesInvoiceView.dart';
+export 'package:capacitors/ui/view/SalesOrderView.dart';
+
+export 'dart:convert';
+export '../../main.dart';
+export 'package:flutter/services.dart';
+export 'package:flutter/material.dart';
+export 'package:flutter/rendering.dart';
+export 'package:getflutter/getflutter.dart';
+export 'package:qr_flutter/qr_flutter.dart';
+export 'package:flutter_html/flutter_html.dart';
+export 'package:awesome_dialog/awesome_dialog.dart';
+export 'package:progress_dialog/progress_dialog.dart';
+export 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
